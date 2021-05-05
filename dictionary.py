cc0b730b1625eb461dc90434dd5681b7ea3c2ddf
YEJI <alsdlsghk@naver.com>
2021-05-05 20:45:31 +0900

    zz

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+pairs = { 'red' : '빨간색', 'blue' : '파랑색', 'green' : '초록색'}
+# #딕셔너리 자료형 : { key : value}
+
+# # 새로운 자료 data를 추가하자
+# pairs['yellow'] - '노랑색'
+# print(pairs)
+
+# #기존 data를 삭제하자
+# # del 딕셔너리형변수[key]으로 표현
+# del pairs['blue']
+# print(pairs)
+
+# #key를 통해서 value를 얻는 함수 .get함수
+# #딕셔너리형변수.get(key)
+color = pairs.get('green')
+#print(color)
+
+print(color)
---
+++ testy.py
@@ -0,0 +1,21 @@
+
+#1번
+
+money = int(input("머니를 입력하세요")) 
+def func(money): 
+if money >= 10000:
+  print("고기를 먹는다") 
+elif (5000<money):
+  print("국밥을 먹는다")
+else: print("라면을 먹는다") 
+func(money)
+
+
+
+#2번
+
+
+
+fruits = ["딸기","수박","멜론","사과","포도"] for i in rang (6): 
+fruit = input(i, "과일을 입력하세요.") 
+for fruit in fruit: print("번째 과일은 입니다.")
